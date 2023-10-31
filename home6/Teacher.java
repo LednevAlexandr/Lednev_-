2e26aefa2e2649750a13af3c09d3d72023e716b9
LednevAlexandr <92679120+LednevAlexandr@users.noreply.github.com>
2023-10-31 20:21:36 +0300

    Update Teacher.java

--- home6/Teacher.java
+++ home6/Teacher.java
@@ -1,33 +1,17 @@
 package home6;
 
-public class Teacher {
-    String firstName;
-    String lastName;
+public abstract class Teacher {
+//  сделала класс абстрактным
+    protected String firstName;
+    protected String lastName;
+//  сделала поля защищёнными
 
     protected Teacher(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
-    public String getFirstName() {return firstName;}
+// Убрал возможность получать и переписывать защищённые поля (класс закрыт для модификаций, открыт для расширений)
+// Избавился от неиспользуемых функций
 
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    @Override
-    public String toString() {
-        return "Teacher{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
-    }
 }
